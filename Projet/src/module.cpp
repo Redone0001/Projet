a03e4087e48a7a64f75f91fec9e64db2463daf35
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-07 12:14:40 +0100

    Update module.cpp

--- Projet/src/module.cpp
+++ Projet/src/module.cpp
@@ -1,14 +1,18 @@
+#include "../include/module.h"
+using namespace std;
+
 void module::module(float Masse,float Carburant, float coef, float SurfaceFrot):
 masse(Masse),
 carburant(Carburant),
 coefPuissance(coef),
 surfaceFrot(SurfaceFrot)
 {
-consomation = 0;
+    consomation = 0;
 
 }
 
 float module::puissance(){
     float puissance = coefPuissance * consomation;
+    return puissance;
   
 }
