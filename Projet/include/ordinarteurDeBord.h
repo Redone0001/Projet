9fdbbd612ba79b5d7ff2999e60389da635b51d7d
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-07 14:06:25 +0100

    Update ordinarteurDeBord.h

--- Projet/include/ordinarteurDeBord.h
+++ Projet/include/ordinarteurDeBord.h
@@ -1,10 +1,19 @@
-
+#include <utility>
 
 class ordinateurDeBord{
 	public :
-	bool checkCarburant(module);
-	void updateCarburant(module);
-	float sumPuissance(vector);
+	bool checkCarburant(module); //vérifie que le carburant est pas trop bas et si c'est à un niveau critique éjecte(supprime) le module
+	void updateCarburant(module);//update le carburant 
+	float sumPuissance(vector);// dans le sens du mouvement
+	void sumForces(); // modifie la pair de float Somme des forces
+	void calculGravite(vector);  // modifie la pair de float gravite
+	float sumFrottement(vector lanceurVec);//dans le sens opposé au mouvement
+	pair <float,float> gravite;
+	pair <float,float> position; // la possition de la fussée est repéré dans un référentiel placé au centre de la terre et immobile
+	pair <float,float> vitesse; // la vitesse est dans le même repère que la possition 
+	pair <float,float> acceleration; //idem
+	pair <float,float> SommeForces;
+	float angle; // angle de la fusée / à la verticale
 	
 };
 
