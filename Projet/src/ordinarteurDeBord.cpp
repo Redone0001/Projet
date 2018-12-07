849ae86a47fd89869474e56083bc67e60c7fc3bc
Redone0001 <35457642+Redone0001@users.noreply.github.com>
2018-12-07 14:06:22 +0100

    Update ordinarteurDeBord.cpp

--- Projet/src/ordinarteurDeBord.cpp
+++ Projet/src/ordinarteurDeBord.cpp
@@ -1,5 +1,11 @@
 #include "../include/ordinateurDeBord.h"
 #include <vector>
+#include <math.h>
+
+using namespace std;
+ardinateurDeBord::ordidanteurDeBord(){
+		
+}
 
 bool ordidanteurDeBord::checkCarburant(module x){
 	if (x.carburant<1){
@@ -20,5 +26,34 @@ float ordinateurDeBord::sumPuissance(vector lanceurVec){
 		sum += x.puissance();
 	}
 	return sum;
+}
+void ordinateurDeBord::calculGravite(vector lanceurVec){
+	float masseTot;
+	for (auto x:lanceurVec){
+	masseTot+=x.masse;
+	masseTot+=x.carburant;
+		
+	}
+	gravite.first = pow(6.674,pow(10,-11))*masseTot/pow(position.first,2);
+	gravite.second = pow(6.674,pow(10,-11))*masseTot/pow(position.second,2);
+}
+
+float ordinateurDeBord::sumFrottement(vector lanceurVec){
+	for (auto x:lanceurVec){
+		surfaceTot+=x.surfaceDeFrot;
+	}
+	float forceFrot;
+	forceFrot = pow(vitesse.first,2)*pow(vitesse.second,2)*pression()*surfaceTot;
+		
+	return forceFrot;
+	
+}
+
+void ordinateurDeBord::sumForce(vector lanceurVec){
+	calculGravite(lanceurVec);
+	sumPuissance(lanceurVec);
+	sumFrottement(lanceurVec);
+	
+	
 }
 
